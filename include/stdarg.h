d2291b091004d6e2c4cb72296f359e58d3aa7f81
Niclas Overby <niclas@overby.me>
2026-03-21 09:43:15 +0100

    feat(rust/nixpkgs): Wire rust-gcc into nixpkgs with cc-wrapper, C compilation works
    Signed-off-by: Niclas Overby <niclas@overby.me>

---
+++ include/stdarg.h
@@ -0,0 +1,13 @@
+/* stdarg.h - stub for rust-gcc (definitions provided by compiler builtins) */
+#ifndef _STDARG_H
+#define _STDARG_H
+
+typedef __builtin_va_list va_list;
+typedef __builtin_va_list __gnuc_va_list;
+
+#define va_start(ap, last) __builtin_va_start(ap, last)
+#define va_end(ap)         __builtin_va_end(ap)
+#define va_arg(ap, type)   __builtin_va_arg(ap, type)
+#define va_copy(dest, src) __builtin_va_copy(dest, src)
+
+#endif /* _STDARG_H */
---
+++ include/stddef.h
@@ -0,0 +1,23 @@
+/* stddef.h - stub for rust-gcc (definitions provided by compiler builtins) */
+#ifndef _STDDEF_H
+#define _STDDEF_H
+
+typedef __SIZE_TYPE__ size_t;
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+
+#ifndef __cplusplus
+typedef __WCHAR_TYPE__ wchar_t;
+#endif
+
+#undef NULL
+#ifdef __cplusplus
+#define NULL __null
+#else
+#define NULL ((void *)0)
+#endif
+
+#define offsetof(type, member) __builtin_offsetof(type, member)
+
+typedef long max_align_t;
+
+#endif /* _STDDEF_H */
